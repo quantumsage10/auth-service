ede82a0d39579abcdc2580cb947c68585daebc0a
Runi <13runipanda@gmail.com>
2025-01-04 19:15:50 +0530

    Hashed the password & saved to database

--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -3,17 +3,21 @@ import { User } from '../entity/User'
 import { UserData } from '../types'
 import createHttpError from 'http-errors'
 import { Roles } from '../constants'
+import bcrypt from 'bcrypt'
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
 
     async create({ firstName, lastName, email, password }: UserData) {
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10)
+
         try {
             return await this.userRepository.save({
                 firstName,
                 lastName,
                 email,
-                password,
+                password: hashedPassword,
                 role: Roles.CUSTOMER,
             })
         } catch (err) {
