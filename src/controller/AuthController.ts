54a8c9e62d7b2bb6b191fc263d296bb928cdaa7d
Runi <13runipanda@gmail.com>
2025-01-04 15:04:49 +0530

    Store/Persist Data in Database

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from 'express'
+import { AppDataSource } from '../config/data-source'
+import { User } from '../entity/User'
+
+interface UserData {
+    firstName: string
+    lastName: string
+    email: string
+    password: string
+}
+
+interface RegisterUserRequest extends Request {
+    body: UserData
+}
 
 export class AuthController {
-    register(req: Request, res: Response) {
-        res.status(201).json({ message: 'User registered successfully' })
+    async register(req: RegisterUserRequest, res: Response) {
+        console.log('Body', req.body)
+        const { firstName, lastName, email, password } = req.body
+        const userRepository = AppDataSource.getRepository(User)
+
+        await userRepository.save({ firstName, lastName, email, password })
+        res.status(201).json()
     }
 }
