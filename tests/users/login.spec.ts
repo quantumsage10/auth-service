9ec9fd63d4bb4b41db40ff7d2769b213b34c402b
Runi <13runipanda@gmail.com>
2025-01-06 19:23:50 +0530

    TDD applied  fisrt for login user

---
+++ tests/users/login.spec.ts
@@ -0,0 +1,39 @@
+import { DataSource } from 'typeorm'
+import { AppDataSource } from '../../src/config/data-source'
+import request from 'supertest'
+import app from '../../src/app'
+
+describe('POST/auth/login', () => {
+    let connection: DataSource
+
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize()
+    })
+    beforeEach(async () => {
+        await connection.dropDatabase()
+        await connection.synchronize()
+    })
+    afterAll(async () => {
+        await connection.destroy()
+    })
+
+    describe('Given all Fields', () => {
+        it('should login the user & return status code 201', async () => {
+            // Arrange
+            const userData = {
+                firstName: 'runi',
+                lastName: 'p',
+                email: 'runi@mern.space',
+                password: 'secret',
+            }
+
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            // Assert
+            expect(response.statusCode).toBe(201)
+        })
+    })
+})
