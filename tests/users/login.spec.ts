2cb0ff8850b28e668d37baaffbc3342e3d6ef7c0
Runi <13runipanda@gmail.com>
2025-01-06 20:50:01 +0530

    add  jest test for login endpoint

--- tests/users/login.spec.ts
+++ tests/users/login.spec.ts
@@ -2,6 +2,9 @@ import { DataSource } from 'typeorm'
 import { AppDataSource } from '../../src/config/data-source'
 import request from 'supertest'
 import app from '../../src/app'
+import { User } from '../../src/entity/User'
+import { Roles } from '../../src/constants'
+import bcrypt from 'bcrypt'
 
 describe('POST/auth/login', () => {
     let connection: DataSource
@@ -21,17 +24,29 @@ describe('POST/auth/login', () => {
         it('should login the user & by default status code is 200', async () => {
             // Arrange
             const userData = {
+                firstName: 'runi',
+                lastName: 'p',
                 email: 'runi@mern.space',
                 password: 'secret',
             }
 
+            // hash the passowrd & store in the db
+            const hashedPassword = await bcrypt.hash(userData.password, 10)
+
+            const userRepository = connection.getRepository(User)
+            await userRepository.save({
+                ...userData,
+                password: hashedPassword,
+                role: Roles.CUSTOMER,
+            })
+
             // Act
             const response = await request(app)
                 .post('/auth/login')
-                .send(userData)
+                .send({ email: userData.email, password: userData.password })
 
             // Assert
-            expect(response.badRequest).toBe(true)
+            expect(response.statusCode).toBe(200)
         })
     })
 })
